d3f9ac166b2ab837984077b4ab642376673905e4
goshak24 <166661419+goshak24@users.noreply.github.com>
2026-01-12 10:47:57 +0000

    Walking up and down animation with for loop rendering animation images

--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,5 @@
 #include "player.hpp"
-#include "texture_manager.hpp"
+#include "texture_manager.hpp" 
 
 Player::Player(const char* textureSheet, int x, int y)
     : GameObject(textureSheet, x, y)
@@ -10,7 +10,31 @@ Player::Player(const char* textureSheet, int x, int y)
     idle0 = TextureManager::LoadTexture("assets/character/idle/tile000.png");
     idle1 = TextureManager::LoadTexture("assets/character/idle/tile003.png");
     idle2 = TextureManager::LoadTexture("assets/character/idle/tile002.png");
-}
+
+    walk_down0 = TextureManager::LoadTexture("assets/character/moving/walk/tile000.png");
+    walk_down1 = TextureManager::LoadTexture("assets/character/moving/walk/tile001.png");
+    walk_down2 = TextureManager::LoadTexture("assets/character/moving/walk/tile002.png");
+    walk_down3 = TextureManager::LoadTexture("assets/character/moving/walk/tile003.png");
+
+    walk_up0 = TextureManager::LoadTexture("assets/character/moving/walk/tile018.png");
+    walk_up1 = TextureManager::LoadTexture("assets/character/moving/walk/tile019.png");
+    walk_up2 = TextureManager::LoadTexture("assets/character/moving/walk/tile020.png");
+    walk_up3 = TextureManager::LoadTexture("assets/character/moving/walk/tile021.png");
+
+    walkDown = {
+        walk_down0,
+        walk_down1,
+        walk_down2,
+        walk_down3
+    };
+
+    walkUp = {
+        walk_up0,
+        walk_up1,
+        walk_up2,
+        walk_up3
+    };
+}    
 
 void Player::handleKeyboardPress(Uint8 event) 
 {
@@ -20,10 +44,12 @@ void Player::handleKeyboardPress(Uint8 event)
     {
         case SDL_SCANCODE_W:
             setY(getY() - 10);
+            updateMove(walkUp);
             moved = true;
             break;
         case SDL_SCANCODE_S:
             setY(getY() + 10);
+            updateMove(walkDown);
             moved = true;
             break;
         case SDL_SCANCODE_A:
@@ -39,12 +65,9 @@ void Player::handleKeyboardPress(Uint8 event)
     }
 
     if (moved) 
-    {
-        lastMoveTime = std::chrono::steady_clock::now();
-
+    { 
         // reset idle animation when moving
         idleFrame = 0;
-        setTex(idle0);
     }
 }
 
@@ -54,17 +77,36 @@ void Player::updateIdle()
     auto elapsed =
         std::chrono::duration_cast<std::chrono::seconds>(now - lastMoveTime);
 
+    if (elapsed.count() >= 0.05) 
+    {   
+        setTex(idle0);
+    }
+
     // after 2 seconds → idle frame 1
-    if (elapsed.count() >= 2 && idleFrame == 0) 
+    if (elapsed.count() >= 4 && idleFrame == 0) 
     {
         idleFrame = 1;
         setTex(idle1);
     }
 
     // after 4 seconds → idle frame 2
-    else if (elapsed.count() >= 4 && idleFrame == 1) 
+    else if (elapsed.count() >= 6 && idleFrame == 1) 
     {
         idleFrame = 2;
         setTex(idle2);
+    }
+} 
+
+void Player::updateMove(std::vector<SDL_Texture*>& textures)
+{
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMoveTime);
+
+    if (elapsed.count() > 150)
+    {
+        walkFrame = (walkFrame + 1) % textures.size(); // keeps frames less than texture size 
+        setTex(textures[walkFrame]);
+        lastMoveTime = now;
     }
 } 
--- src/player.hpp
+++ src/player.hpp
@@ -10,14 +10,30 @@ public:
     void handleKeyboardPress(Uint8 event);
     void updateIdle();
 
+    void updateMove(std::vector<SDL_Texture*>& textures); 
+
 private:
     std::chrono::steady_clock::time_point lastMoveTime;
 
     // idle animation state
     int idleFrame = 0;
+    int walkFrame = 0; 
+
+    std::vector<SDL_Texture*> walkDown;
+    std::vector<SDL_Texture*> walkUp;
 
     // cached textures (loaded once)
     SDL_Texture* idle0;
     SDL_Texture* idle1;
     SDL_Texture* idle2;
+
+    SDL_Texture* walk_down0;
+    SDL_Texture* walk_down1;
+    SDL_Texture* walk_down2;
+    SDL_Texture* walk_down3;
+
+    SDL_Texture* walk_up0;
+    SDL_Texture* walk_up1;
+    SDL_Texture* walk_up2;
+    SDL_Texture* walk_up3;
 };
